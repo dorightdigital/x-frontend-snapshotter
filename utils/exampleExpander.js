7b5d6f42bc5c2aae5bcdc218addd412b2f3f849f
Silas <1029540+silasl@users.noreply.github.com>
2019-08-29 12:25:14 +0100

    Fixed deduper

--- utils/exampleExpander.js
+++ utils/exampleExpander.js
@@ -1,14 +1,8 @@
-const stringify = obj => JSON.stringify(obj, Object.keys(obj).sort());
+const sortObjectKeys = obj => JSON.parse(JSON.stringify(obj, Object.keys(obj).sort()))
 
 const removeDuplicates = arr => {
-  arr.map(stringify).sort()
-  let prev
-  arr.filter(current => {
-    const ans = current !== prev
-    prev = current
-    return ans
-  })
-  return arr
+  const dedupe = arr.map(sortObjectKeys)
+  return [...new Set(dedupe)]
 }
 
 const expandExamples = examples => {
